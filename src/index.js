f593a14e9e56004e4d936c4b006126b06b3e7d53
Cindy Chen <cchen4@cnm.edu>
2021-02-17 14:23:42 -0700

    added docker files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
-require('dotenv').config()
 const Recaptcha = require("express-recaptcha").RecaptchaV2
 const mailgun = require("mailgun-js")
 const {check, validationResult} = require("express-validator")
